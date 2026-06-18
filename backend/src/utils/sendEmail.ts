e48fc009e2163b422d0f0aca640632431205f645
Shreya <siyap2474@gmail.com>
2026-06-18 16:04:55 +0530

    fix nodemailer smtp port

--- backend/src/utils/sendEmail.ts
+++ backend/src/utils/sendEmail.ts
@@ -5,13 +5,17 @@ export const sendEmail = async (
   subject: string,
   html: string
 ) => {
+
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
+
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
